3b52c387bf80f3fdc7311f48affaffbce094e534
Quang Phan <vnphanquang@gmail.com>
2022-09-13 20:27:26 +0700

    fix(playground): toc svelte kit migration import context module

--- apps/svelte-kit/src/routes/toc/+page.svelte
+++ apps/svelte-kit/src/routes/toc/+page.svelte
@@ -1,14 +1,7 @@
-<script lang="ts" context="module">
-  throw new Error(
-    '@migration task: Check code was safely removed (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292722)',
-  );
-
-  // import { toc } from '@svelte-put/toc';
-  // import type { TocEventDetails } from '@svelte-put/toc';
-  // import Toc from '@svelte-put/toc/Toc.svelte';
-</script>
-
 <script lang="ts">
+  import { toc } from '@svelte-put/toc';
+  import type { TocEventDetails } from '@svelte-put/toc';
+  import Toc from '@svelte-put/toc/Toc.svelte';
   function onToc(e: CustomEvent<TocEventDetails>) {
     const { items } = e.detail;
   }
--- apps/svelte-kit/src/routes/toc/+page.ts
+++
@@ -1,3 +0,0 @@
-import { toc } from '@svelte-put/toc';
-import type { TocEventDetails } from '@svelte-put/toc';
-import Toc from '@svelte-put/toc/Toc.svelte';
