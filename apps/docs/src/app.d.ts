f8a78fb1e4856a13ca6485ee61797edb7209d51b
Quang Phan <vnphanquang@gmail.com>
2022-10-20 20:44:39 +0700

    chore(docs): reference ambient typing from vite & svelte

--- apps/docs/src/app.d.ts
+++ apps/docs/src/app.d.ts
@@ -1,3 +1,7 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
--- apps/docs/src/routes/+page.svelte
+++ apps/docs/src/routes/+page.svelte
@@ -1,1 +1,4 @@
+<script lang="ts">
+</script>
+
 # Heading 1 ## Test Test Test
