6df4ce29bf2ce79386379cd717b0bfd5269e1688
Quang Phan <vnphanquang@gmail.com>
2022-10-21 18:31:56 +0700

    feat(docs): bootstrap docs site skeleton

---
+++ apps/docs/src/routes/docs/+layout.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  const contents = [
+    {
+      category: 'Actions',
+      packages: [
+        {
+          name: '@svelte-put/clickoutside',
+        },
+        {
+          name: '@svelte-put/intersect',
+        },
+        {
+          name: '@svelte-put/movable',
+        },
+        {
+          name: '@svelte-put/shortcut',
+        },
+        {
+          name: '@svelte-put/toc',
+        },
+      ],
+    },
+    {
+      category: 'Components',
+      packages: [
+        {
+          name: '@svelte-put/select',
+        },
+      ],
+    },
+    {
+      category: 'Miscellaneous',
+      packages: [
+        {
+          name: '@svelte-put/avatar',
+        },
+        {
+          name: '@svelte-put/modal',
+        },
+      ],
+    },
+  ] as const;
+
+  function generateDocHref(packageName: string) {
+    return `/docs/${packageName}`;
+  }
+</script>
+
+<div class="flex-2 min-h-screen">
+  <nav id="navbar" class="flex items-center justify-between border-b border-border py-2 px-6">
+    <p>Logo will go here</p>
+    <a href="https://github.com/vnphanquang/svelte-put" target="__blank" class="c-link">
+      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
+        ><title>GitHub</title><path
+          fill="currentColor"
+          d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+        /></svg
+      >
+    </a>
+  </nav>
+
+  <nav data-sveltekit-prefetch id="sidebar" class="max-w-xs border-r border-border py-6">
+    <ul>
+      <li>
+        <a
+          href="/docs"
+          data-active={$page.url.pathname === '/docs'}
+          class="c-link-bg block py-2 px-6 font-bold"
+        >
+          Introduction
+        </a>
+      </li>
+      {#each contents as { category, packages }}
+        <li class="py-2">
+          <p class="mx-6 font-bold">{category}</p>
+          <ul>
+            {#each packages as { name }}
+              {@const href = generateDocHref(name)}
+              <li>
+                <a
+                  {href}
+                  data-active={$page.url.pathname.includes(name)}
+                  class="c-link-bg block py-2 px-10 text-sm"
+                >
+                  <span class="h-full w-1 bg-primary" />
+                  {name}
+                </a>
+                <!-- <ul>
+                  Get headings from toc automatically and set to some store/context?
+                  {#each headings as h}
+                    <li>
+                      <a href="{href}#{h}" class="block py-2 px-14 text-sm c-link-bg">Installation</a>
+                    </li>
+                  {/each}
+                </ul> -->
+              </li>
+            {/each}
+          </ul>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+  console.log(`Turbo ~ file: +layout.svelte ~ line 91 ~ nav`, nav);
+
+  <main class="c-container justify-self-stretch py-10">
+    <slot />
+  </main>
+</div>
+
+<style lang="postcss">
+  div {
+    @apply grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: auto 1fr;
+    grid-template-areas:
+      'navbar navbar'
+      'sidebar content';
+  }
+
+  nav#navbar {
+    grid-area: navbar;
+  }
+
+  nav#sidebar {
+    grid-area: sidebar;
+  }
+
+  main {
+    grid-area: content;
+  }
+</style>
--- apps/docs/tailwind.config.cjs
+++ apps/docs/tailwind.config.cjs
@@ -1,38 +1,63 @@
-/** @type {import("tailwindcss").Config } */
-const config = {
-  content: ['./src/**/*.{html,js,svelte,ts,md}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        paper: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23000000' fill-opacity='0.02'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+const plugin = require('tailwindcss/plugin');
+
+const sveltePut = plugin(
+  ({ addComponents }) => {
+    addComponents({
+      '.c-container': {
+        '@apply mx-auto w-full max-w-5xl max-md:px-10': {},
       },
-      colors: {
-        fg: '#3c3836',
-        bg: {
-          DEFAULT: '#fbf1c7',
-          accent: '#ebdbb2',
+      '.c-link': {
+        '@apply hover:text-primary': {},
+      },
+      '.c-link-bg': {
+        '@apply data-active:text-primary data-active:hover:text-white hover:bg-primary hover:text-white':
+          {},
+      },
+    });
+  },
+  {
+    theme: {
+      extend: {
+        data: {
+          active: 'active="true"',
         },
-        border: '#d5c4a1',
-        primary: '#d65d03',
-      },
-      keyframes: {
-        'fade-in-up': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(100px)',
+        backgroundImage: {
+          paper: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23000000' fill-opacity='0.02'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+        },
+        colors: {
+          fg: '#3c3836',
+          bg: {
+            DEFAULT: '#fbf1c7',
+            accent: '#ebdbb2',
           },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
+          border: '#d5c4a1',
+          primary: '#d65d03',
+          secondary: '#98971a',
+        },
+        keyframes: {
+          'fade-in-up': {
+            '0%': {
+              opacity: '0',
+              transform: 'translateY(100px)',
+            },
+            '100%': {
+              opacity: '1',
+              transform: 'translateY(0)',
+            },
           },
         },
-      },
-      animation: {
-        'fade-in-up': 'fade-in-up 500ms ease-out',
+        animation: {
+          'fade-in-up': 'fade-in-up 500ms ease-out',
+        },
       },
     },
   },
-  plugins: [],
+);
+
+/** @type {import("tailwindcss").Config } */
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts,md}'],
+  plugins: [sveltePut],
 };
 
 module.exports = config;
