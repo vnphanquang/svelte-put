52328361b3baf580017b99201eab0949bf8e3b09
Quang Phan <vnphanquang@gmail.com>
2022-10-22 18:53:28 +0700

    style(docs): horizontal swipe gradient bar on top on page load

--- apps/docs/src/routes/docs/+layout.svelte
+++ apps/docs/src/routes/docs/+layout.svelte
@@ -55,11 +55,46 @@
   const TRANSITION_DURATION = 200;
   const yIn = 10;
   const yOut = -10;
+
+  // TODO: extract this to own @svelte-put/transition
+  function slide(node: HTMLElement, { delay = 0, duration = 400, easing = cubicOut, axis = 'y' }) {
+    const style = getComputedStyle(node);
+    const opacity = +style.opacity;
+    const primary_dimension = axis === 'y' ? 'height' : 'width';
+    const primary_dimension_value = parseFloat(style[primary_dimension]);
+    const secondary_dimensions =
+      axis === 'y' ? (['Top', 'Bottom'] as const) : (['Left', 'Right'] as const);
+    const padding_start_value = parseFloat(style.padding + secondary_dimensions[0]);
+    const padding_end_value = parseFloat(style.padding + secondary_dimensions[1]);
+    const margin_start_value = parseFloat(style.margin + secondary_dimensions[0]);
+    const margin_end_value = parseFloat(style.margin + secondary_dimensions[1]);
+    const border_width_start_value = parseFloat(style[`border${secondary_dimensions[0]}Width`]);
+    const border_width_end_value = parseFloat(style[`border${secondary_dimensions[1]}Width`]);
+    return {
+      delay,
+      duration,
+      easing,
+      css: (t: number) =>
+        'overflow: hidden;' +
+        `opacity: ${Math.min(t * 20, 1) * opacity};` +
+        `${primary_dimension}: ${t * primary_dimension_value}px;` +
+        `padding-${secondary_dimensions[0].toLowerCase()}: ${t * padding_start_value}px;` +
+        `padding-${secondary_dimensions[1].toLowerCase()}: ${t * padding_end_value}px;` +
+        `margin-${secondary_dimensions[0].toLowerCase()}: ${t * margin_start_value}px;` +
+        `margin-${secondary_dimensions[1].toLowerCase()}: ${t * margin_end_value}px;` +
+        `border-${secondary_dimensions[0].toLowerCase()}-width: ${
+          t * border_width_start_value
+        }px;` +
+        `border-${secondary_dimensions[1].toLowerCase()}-width: ${t * border_width_end_value}px;`,
+    };
+  }
 </script>
 
 <div class="grid min-h-screen flex-1 overflow-y-scroll" id="site-wrapper">
   <nav id="navbar" class="border-b border-border">
-    <div class="h-px w-full bg-gradient-brand" />
+    {#key data.pathname}
+      <div class="h-0.5 w-full bg-gradient-brand" in:slide={{ axis: 'x' }} />
+    {/key}
     <div class="flex w-full items-center justify-between py-2 px-6">
       <a href="/" class="flex items-center gap-2">
         <img src="/images/svelte-put-logo.svg" alt="svelte-put" width="32" height="32" />
