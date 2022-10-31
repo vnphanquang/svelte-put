32b6588a3ba9c7e8507b0b1313289fceae09bebb
Quang Phan <phan.quang@4digit.jp>
2022-10-31 17:52:53 +0700

    feat(docs): add toggle icon btn to Code

--- apps/docs/src/lib/ui/components/Code/Code.svelte
+++ apps/docs/src/lib/ui/components/Code/Code.svelte
@@ -3,11 +3,13 @@
   import type { ComponentProps } from 'svelte';
   import Highlight from 'svelte-highlight/Highlight.svelte';
   import HighlightSvelte from 'svelte-highlight/HighlightSvelte.svelte';
-  import { fade } from 'svelte/transition';
+  import { fade, slide } from 'svelte/transition';
 
   export let lang: ComponentProps<Highlight>['language'] | 'svelte' = 'svelte';
   export let code: string;
   export let title = '';
+  export let expanded = true;
+  export let expansion: 'disabled' | 'enabled' = 'enabled';
 
   let copyBtn: HTMLButtonElement;
 
@@ -17,6 +19,11 @@
   }
   function onMouseLeave() {
     copied = false;
+  }
+
+  function toggleExpansion() {
+    if (expansion === 'disabled') return;
+    expanded = !expanded;
   }
 </script>
 
@@ -28,15 +35,11 @@
     <p class="flex-1 font-fira text-sm">
       {title}
     </p>
-    <button bind:this={copyBtn} class="flex items-center justify-center space-x-1">
+    <button bind:this={copyBtn} class="flex items-center justify-center space-x-1" alt="copy code">
       {#if copied}
-        <span transition:fade={{ duration: 150 }} class="rounded bg-border/10 p-2 text-sm">
-          Copied
-        </span>
+        <span transition:fade={{ duration: 150 }} class="rounded p-2 text-sm"> Copied </span>
       {/if}
-      <span
-        class="flex rounded p-2 hover:bg-border/10 {copied ? 'bg-border/10' : ''} active:scale-95"
-      >
+      <span class="flex rounded p-2 hover:bg-border/10 active:scale-95">
         {#if copied}
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -68,23 +71,42 @@
         {/if}
       </span>
     </button>
-  </div>
-  <div use:copy={{ trigger: copyBtn }} on:copy={onCopy}>
-    {#if lang === 'svelte'}
-      <HighlightSvelte {code} langtag />
-    {:else}
-      <Highlight language={lang} {code} langtag />
+    {#if expansion !== 'disabled'}
+      <button
+        alt="collapse code block"
+        on:click={toggleExpansion}
+        class="flex rounded p-2 hover:bg-border/10"
+      >
+        <svg
+          class="inline-block transition-transform"
+          class:rotate-180={!expanded}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 48 48"
+          height="20"
+          width="20"
+        >
+          <path d="M14.15 30.75 12 28.6l12-12 12 11.95-2.15 2.15L24 20.85Z" fill="currentColor" />
+        </svg>
+      </button>
     {/if}
   </div>
+  {#if expanded}
+    <div use:copy={{ trigger: copyBtn }} on:copy={onCopy} transition:slide={{ duration: 150 }}>
+      {#if lang === 'svelte'}
+        <HighlightSvelte {code} langtag />
+      {:else}
+        <Highlight language={lang} {code} langtag />
+      {/if}
+    </div>
+  {/if}
 </div>
 
 <style lang="postcss">
-  /* move global style for code here */
   :global(pre:not(#fakeId)) {
     @apply m-0 rounded-none border-t border-border p-0;
-    &::after {
-      @apply p-4;
-    }
+  }
+  :global(pre:not(#fakeId)::after) {
+    @apply p-4;
   }
   :global(pre:not(#fakeId) code) {
     @apply p-6 font-fira;
