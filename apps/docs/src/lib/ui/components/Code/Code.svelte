6d31c895b30b78c47dd986baa48d45e2d9c69fe3
Quang Phan <phan.quang@4digit.jp>
2022-11-01 11:30:57 +0700

    refactor(docs.Code): simplify and localize transition

--- apps/docs/src/lib/ui/components/Code/Code.svelte
+++ apps/docs/src/lib/ui/components/Code/Code.svelte
@@ -37,38 +37,30 @@
     </p>
     <button bind:this={copyBtn} class="flex items-center justify-center space-x-1" alt="copy code">
       {#if copied}
-        <span transition:fade={{ duration: 150 }} class="rounded p-2 text-sm"> Copied </span>
+        <span transition:fade|local={{ duration: 150 }} class="rounded p-2 text-sm"> Copied </span>
       {/if}
       <span class="flex rounded p-2 hover:bg-border/10 active:scale-95">
-        {#if copied}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
-            height="20"
-            width="20"
-            class="inline-block"
-            in:fade={{ duration: 150 }}
-          >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 48 48"
+          height="20"
+          width="20"
+          class="inline-block"
+        >
+          {#if copied}
             <path
               d="M18.9 35.7 7.7 24.5l2.15-2.15 9.05 9.05 19.2-19.2 2.15 2.15Z"
               fill="currentColor"
+              in:fade|local={{ duration: 150 }}
             />
-          </svg>
-        {:else}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
-            height="20"
-            width="20"
-            class="inline-block"
-            in:fade={{ duration: 150 }}
-          >
+          {:else}
             <path
               d="M9 43.95q-1.2 0-2.1-.9-.9-.9-.9-2.1V10.8h3v30.15h23.7v3Zm6-6q-1.2 0-2.1-.9-.9-.9-.9-2.1v-28q0-1.2.9-2.1.9-.9 2.1-.9h22q1.2 0 2.1.9.9.9.9 2.1v28q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h22v-28H15v28Zm0 0v-28 28Z"
               fill="currentColor"
+              in:fade|local={{ duration: 150 }}
             />
-          </svg>
-        {/if}
+          {/if}
+        </svg>
       </span>
     </button>
     {#if expansion !== 'disabled'}
@@ -91,7 +83,11 @@
     {/if}
   </div>
   {#if expanded}
-    <div use:copy={{ trigger: copyBtn }} on:copy={onCopy} transition:slide={{ duration: 150 }}>
+    <div
+      use:copy={{ trigger: copyBtn }}
+      on:copy={onCopy}
+      transition:slide|local={{ duration: 150 }}
+    >
       {#if lang === 'svelte'}
         <HighlightSvelte {code} langtag />
       {:else}
