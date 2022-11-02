05a01dfdac404710754f6ba34abfd950207799a1
Quang Phan <phan.quang@4digit.jp>
2022-11-02 10:48:39 +0700

    chore(docs): setup some material icon as svelte components

---
+++ apps/docs/src/lib/ui/components/icons/ExpandLess.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { ComponentProps } from 'svelte';
+
+  import Svg from './Svg.svelte';
+
+  type $$Props = ComponentProps<Svg>;
+</script>
+
+<Svg width="20" height="20" {...$$restProps}>
+  <path d="M14.15 30.75 12 28.6l12-12 12 11.95-2.15 2.15L24 20.85Z" />
+</Svg>
