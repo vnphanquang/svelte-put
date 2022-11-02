7e2ebd0b79721284c1c2e8a0040ff9810b77890e
Quang Phan <phan.quang@4digit.jp>
2022-11-02 10:48:39 +0700

    chore(docs): setup some material icon as svelte components

---
+++ apps/docs/src/lib/ui/components/icons/Done.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { ComponentProps } from 'svelte';
+
+  import Svg from './Svg.svelte';
+
+  type $$Props = ComponentProps<Svg>;
+</script>
+
+<Svg width="20" height="20" {...$$restProps}>
+  <path d="M18.9 35.7 7.7 24.5l2.15-2.15 9.05 9.05 19.2-19.2 2.15 2.15Z" />
+</Svg>
---
+++ apps/docs/src/lib/ui/components/icons/Svg.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  // eslint-disable-next-line no-undef
+  type $$Props = svelteHTML.IntrinsicElements['svg'];
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" {...$$restProps}>
+  <slot />
+</svg>
