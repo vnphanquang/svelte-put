e15b874edd2281372de20807d5f1aeddc87012ba
Quang Phan <phan.quang@4digit.jp>
2022-11-02 10:55:03 +0700

    feat(docs.components.Code): add optional indicator icon to title

--- apps/docs/src/lib/ui/components/Code/Code.svelte
+++ apps/docs/src/lib/ui/components/Code/Code.svelte
@@ -5,11 +5,19 @@
   import HighlightSvelte from 'svelte-highlight/HighlightSvelte.svelte';
   import { fade, slide } from 'svelte/transition';
 
+  import IconCode from '$lib/ui/components/icons/Code.svelte';
+  import IconCopy from '$lib/ui/components/icons/Copy.svelte';
+  import IconDone from '$lib/ui/components/icons/Done.svelte';
+  import IconError from '$lib/ui/components/icons/Error.svelte';
+  import IconExpandLess from '$lib/ui/components/icons/ExpandLess.svelte';
+  import IconWarning from '$lib/ui/components/icons/Warning.svelte';
+
   export let lang: ComponentProps<Highlight>['language'] | 'svelte' = 'svelte';
   export let code: string;
   export let title = '';
   export let expanded = true;
   export let expansion: 'disabled' | 'enabled' = 'enabled';
+  export let icon: 'none' | 'code' | 'warning' | 'error' = 'code';
 
   let copyBtn: HTMLButtonElement;
 
@@ -28,57 +36,48 @@
 </script>
 
 <div
-  class="code-wrapper group relative my-6 overflow-hidden rounded-md text-[#e5e7eb] shadow hover:shadow-md {$$props.class}"
+  class="code-wrapper group relative my-6 overflow-hidden rounded-md text-code-fg shadow hover:shadow-md {$$props.class}"
   on:mouseleave={onMouseLeave}
 >
-  <div class="not-prose flex items-center bg-[#32302f]/90 py-2 pl-6 pr-4">
-    <p class="flex-1 font-fira text-sm">
-      {title}
+  <div class="not-prose flex items-center bg-code-bg py-2 pl-6 pr-4">
+    <p class="flex flex-1 items-center gap-x-2 font-fira text-sm">
+      <span class="w-6">
+        {#if icon === 'code'}
+          <IconCode />
+        {:else if icon === 'warning'}
+          <IconWarning class="text-yellow-500" />
+        {:else if icon === 'error'}
+          <IconError class="text-red-500" />
+        {/if}
+      </span>
+      <span>
+        {title}
+      </span>
     </p>
     <button bind:this={copyBtn} class="flex items-center justify-center space-x-1" alt="copy code">
       {#if copied}
         <span transition:fade|local={{ duration: 150 }} class="rounded p-2 text-sm"> Copied </span>
       {/if}
-      <span class="flex rounded p-2 hover:bg-border/10 active:scale-95">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 48 48"
-          height="20"
-          width="20"
-          class="inline-block"
+      {#key copied}
+        <span
+          class="flex rounded p-2 hover:bg-border/10 active:scale-95"
+          in:fade|local={{ duration: 150 }}
         >
           {#if copied}
-            <path
-              d="M18.9 35.7 7.7 24.5l2.15-2.15 9.05 9.05 19.2-19.2 2.15 2.15Z"
-              fill="currentColor"
-              in:fade|local={{ duration: 150 }}
-            />
+            <IconDone class="inline-block" />
           {:else}
-            <path
-              d="M9 43.95q-1.2 0-2.1-.9-.9-.9-.9-2.1V10.8h3v30.15h23.7v3Zm6-6q-1.2 0-2.1-.9-.9-.9-.9-2.1v-28q0-1.2.9-2.1.9-.9 2.1-.9h22q1.2 0 2.1.9.9.9.9 2.1v28q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h22v-28H15v28Zm0 0v-28 28Z"
-              fill="currentColor"
-              in:fade|local={{ duration: 150 }}
-            />
+            <IconCopy class="inline-block" />
           {/if}
-        </svg>
-      </span>
+        </span>
+      {/key}
     </button>
     {#if expansion !== 'disabled'}
       <button
         alt="collapse code block"
         on:click={toggleExpansion}
-        class="flex rounded p-2 hover:bg-border/10"
+        class="flex rounded p-2 hover:bg-border/10 active:scale-95"
       >
-        <svg
-          class="inline-block transition-transform"
-          class:rotate-180={!expanded}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 48 48"
-          height="20"
-          width="20"
-        >
-          <path d="M14.15 30.75 12 28.6l12-12 12 11.95-2.15 2.15L24 20.85Z" fill="currentColor" />
-        </svg>
+        <IconExpandLess class="inline-block transition-transform {!expanded ? 'rotate-180' : ''}" />
       </button>
     {/if}
   </div>
