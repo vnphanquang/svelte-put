ea8165cc0d5cf6b33a7524cb93c57d4b180e112f
Quang Phan <phan.quang@4digit.jp>
2022-11-02 10:56:11 +0700

    refactor(docs): inline code style overrides and extracting common sections

---
+++ apps/docs/src/lib/ui/components/ActionUsageNotice/ActionUsageNotice.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Code from '$lib/ui/components/Code/Code.svelte';
+  import ResourceLink from '$lib/ui/components/ResourceLink/ResourceLink.svelte';
+  export let action: string;
+
+  const code = `<-- correct usage-->
+<div use:${action} />
+
+<-- incorrect usage-->
+<Component use:${action} />`;
+</script>
+
+<h2>Action Usage Notice</h2>
+
+As with any <ResourceLink key="svelte action" />, <code>clickoutside</code> should be use with
+<code>element</code>
+and not <code>component</code>.
+
+<Code lang="svelte" {code} title="use:action is an element directive" icon="warning" />
---
+++ apps/docs/src/lib/ui/components/Installation/Installation.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import shell from 'svelte-highlight/languages/shell';
+
+  import CodeSwitch from '$lib/ui/components/Code/CodeSwitch.svelte';
+
+  export let pkg: string;
+  export let dev = true;
+</script>
+
+<h2>Installation</h2>
+
+<CodeSwitch
+  codes={[
+    {
+      variant: 'npm',
+      code: `npm install ${dev ? '--save-dev ' : ''}${pkg}`,
+      lang: shell,
+      title: 'terminal',
+    },
+    {
+      variant: 'pnpm',
+      code: `pnpm add ${dev ? '-D ' : ''}${pkg}`,
+      lang: shell,
+      title: 'terminal',
+    },
+    {
+      variant: 'yarn',
+      code: `yarn add ${dev ? '-D ' : ''}${pkg}`,
+      lang: shell,
+      title: 'terminal',
+    },
+  ]}
+/>
