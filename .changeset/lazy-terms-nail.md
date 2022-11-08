cd8d66e9d668e058636eb9c4c9d561c83282adb0
Quang Phan <vnphanquang@gmail.com>
2022-11-08 20:39:34 +0700

    refactor(avatar): automaticall resolve size from config props

---
+++ .changeset/lazy-terms-nail.md
@@ -0,0 +1,5 @@
+---
+'@svelte-put/avatar': patch
+---
+
+automatically assign size from gravatar and uiAvatar if provided
--- packages/misc/avatar/src/lib/Avatar.svelte
+++ packages/misc/avatar/src/lib/Avatar.svelte
@@ -15,7 +15,7 @@
   export let gravatar: GravatarOptions | string | undefined = undefined;
   export let uiAvatar: string | UIAvatarOptions | undefined = undefined;
   export let fallback = DEFINITIVE_FALLBACK;
-  export let size = 32;
+  export let size: number | undefined = undefined;
   export let alt = (function () {
     if (typeof uiAvatar === 'string') {
       return uiAvatar;
@@ -30,6 +30,7 @@
     }
   })();
 
+  $: rSize = resolveSize(32, size, src, gravatar, uiAvatar);
   $: trials = resolve(src, gravatar, uiAvatar, fallback);
   let resolvedSrc = DEFINITIVE_FALLBACK;
 
@@ -46,6 +47,32 @@
       }
     }
   });
+
+  function getSizeFromUrl(url: string): number | undefined {
+    const matchS = url.match(/s=(\d+)/g) ?? [];
+    const matchSize = url.match(/size=(\d+)/g) ?? [];
+    const match = [...matchS, ...matchSize].filter((m) => url[url.indexOf(m) - 1] !== '-');
+    if (match) {
+      return Number(match[0].split('=')[1]);
+    }
+    return undefined;
+  }
+  function resolveSize(
+    fallback: number,
+    _size?: number,
+    src?: string,
+    _gravatar?: typeof gravatar,
+    _uiAvatar?: typeof uiAvatar,
+  ) {
+    try {
+      if (_size) return _size;
+      if (_gravatar && typeof _gravatar === 'object') return _gravatar.size || fallback;
+      if (_uiAvatar && typeof _uiAvatar === 'object') return _uiAvatar.size || fallback;
+      if (src) return getSizeFromUrl(src) || fallback;
+    } catch (error) {
+      return fallback;
+    }
+  }
 
   function nonNullableFilter<TValue>(value: TValue | null | undefined): value is TValue {
     if (value === null || value === undefined) return false;
@@ -85,5 +112,13 @@
   @public
 -->
 <slot src={resolvedSrc}>
-  <img src={resolvedSrc} {alt} height={size} width={size} class={$$props.class} />
+  <img src={resolvedSrc} {alt} height={rSize} width={rSize} class={$$props.class} />
 </slot>
+
+<style>
+  :where(img) {
+    aspect-ratio: 1 / 1;
+    object-fit: cover;
+    object-position: center;
+  }
+</style>
