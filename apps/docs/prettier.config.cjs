a56b8477101590f122972edcff1c34aa87afb5e5
Quang Phan <vnphanquang@gmail.com>
2022-12-03 23:34:24 +0700

    chore: update dependencies

--- apps/docs/prettier.config.cjs
+++ apps/docs/prettier.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   ...require('@vnphanquang/prettierrc'),
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: [require('prettier-plugin-svelte'), require('prettier-plugin-tailwindcss')],
+  pluginSearchDirs: false,
   tailwindConfig: './tailwind.config.cjs',
 };
