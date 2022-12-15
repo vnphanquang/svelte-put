146b9a33a386f357f99ac1004ceb0b8cedf3b171
Quang Phan <phan.quang@4digit.jp>
2022-12-15 07:56:59 +0700

    refactor(docs.hooks): set userId cookie name to a private env

--- apps/docs/src/hooks.server.ts
+++ apps/docs/src/hooks.server.ts
@@ -1,18 +1,20 @@
 import type { Handle } from '@sveltejs/kit';
 import * as cookie from 'cookie';
 
+import { COOKIE_USER_ID } from '$env/static/private';
+
 export const handle: Handle = async ({ event, resolve }) => {
   const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-  event.locals.userId = cookies['userId'] || crypto.randomUUID();
+  event.locals.userId = cookies[COOKIE_USER_ID] || crypto.randomUUID();
 
   const response = await resolve(event);
 
-  if (!cookies['userId']) {
+  if (!cookies[COOKIE_USER_ID]) {
     // if this is the first time the user has visited this app,
     // set a cookie so that we recognise them when they return
     response.headers.set(
       'set-cookie',
-      cookie.serialize('userId', event.locals.userId, {
+      cookie.serialize(COOKIE_USER_ID, event.locals.userId, {
         path: '/',
         httpOnly: true,
       }),
