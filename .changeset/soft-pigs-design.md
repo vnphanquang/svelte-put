dbeecf88701d4e25186757d5e05515f44e0e4968
Quang Phan <vnphanquang@gmail.com>
2023-01-09 22:01:59 +0700

    fix(preprocess-auto-slug): fix text content duplication for slugify

---
+++ .changeset/soft-pigs-design.md
@@ -0,0 +1,5 @@
+---
+'@svelte-put/preprocess-auto-slug': patch
+---
+
+fix: duplication in slug from `getTextContent`; should only walk node once
--- packages/preprocessors/auto-slug/src/auto-slug.utils.ts
+++ packages/preprocessors/auto-slug/src/auto-slug.utils.ts
@@ -26,60 +26,61 @@ export function getIdAttribute(
 /**
  * @internal
  */
-export function getTextContent(node: Node) {
-  if (node.type === 'Text') return node.data;
-  if (node.type === 'MustacheTag') {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const fNode = node as any;
-    switch (fNode.expression.type) {
-      case 'Identifier':
-        return fNode.expression.name;
-      case 'MemberExpression': {
-        const { object, property } = fNode.expression;
-        return `${object.name}-${property.name}`;
-      }
-      case 'CallExpression': {
-        let text = '';
-        switch (fNode.expression.callee.type) {
+export function getTextContent(root: Node) {
+  let text = '';
+  walk(root, {
+    enter(node: Node) {
+      if (node.type === 'Text') text += node.data;
+      if (node.type === 'MustacheTag') {
+        // TODO potential improvement with MustachTag
+        // sometimes this will capture unnecessary text
+        // for example slot="foo" will capture "foo"
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const fNode = node as any;
+        switch (fNode.expression.type) {
+          case 'Identifier':
+            text += fNode.expression.name;
+            break;
           case 'MemberExpression': {
-            const { object, property } = fNode.expression.callee;
-            text = `${object.name}-${property.name}`;
+            const { object, property } = fNode.expression;
+            text += `${object.name}-${property.name}`;
             break;
           }
-          case 'Identifier': {
-            text = fNode.expression.callee.name;
+          case 'CallExpression': {
+            switch (fNode.expression.callee.type) {
+              case 'MemberExpression': {
+                const { object, property } = fNode.expression.callee;
+                text += `${object.name}-${property.name}`;
+                break;
+              }
+              case 'Identifier': {
+                text += fNode.expression.callee.name;
+                break;
+              }
+              default:
+                break;
+            }
+            const args = fNode.expression.arguments;
+            for (const arg of args) {
+              switch (arg.type) {
+                case 'Identifier':
+                  text += `-${arg.name}`;
+                  break;
+                case 'Literal':
+                  text += `-${arg.value}`;
+                  break;
+                default:
+                  break;
+              }
+            }
             break;
           }
           default:
             break;
         }
-        const args = fNode.expression.arguments;
-        for (const arg of args) {
-          switch (arg.type) {
-            case 'Identifier':
-              text += `-${arg.name}`;
-              break;
-            case 'Literal':
-              text += `-${arg.value}`;
-              break;
-            default:
-              break;
-          }
-        }
-        return text;
       }
-      default:
-        return '';
-    }
-  }
-  if (!node.children || node.children.length === 0) return '';
-  let text = '';
-  for (const child of Array.from(node.children)) {
-    walk(child, {
-      enter(childNode) {
-        text += getTextContent(childNode as Node);
-      },
-    });
-  }
+    },
+  });
+
   return text;
 }
