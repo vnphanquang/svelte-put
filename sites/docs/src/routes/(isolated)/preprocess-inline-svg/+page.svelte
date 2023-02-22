bfde1ff38c225762f075cc6c5587a1c3a45f0481
Quang Phan <phan.quang@4digit.jp>
2023-02-22 16:59:04 +0700

    chore(docs): prepare svg icons

---
+++ sites/docs/src/routes/(isolated)/preprocess-inline-svg/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  const attr = 'test-attribute';
+</script>
+
+<svg data-inline-src="./icon.svg" data-attr={attr} width={100} />
+
+<svg data-inline-src="google/account-tree" />
+
+<svg data-inline-src="svelte" width="100" height="100" />
+<svg data-inline-src="simpleicon/tailwindcss" width="100" height="100" />
+<svg data-inline-src="simpleicon/vercel" width="100" height="100" />
+<svg data-inline-src="simpleicon/github" width="100" height="100" />
+<!-- <svg data-inline-src="404" width="100" height="100"></svg> -->
