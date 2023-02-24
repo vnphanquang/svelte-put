d0f907d1bb4742504327e993b4f887c9f158b7f3
Quang Phan <vnphanquang@gmail.com>
2023-02-24 21:04:57 +0700

    chore(docs.ApiReference): prop to turn off / on typescript recommendation

--- sites/docs/src/lib/client/components/ApiReference/ApiReference.svelte
+++ sites/docs/src/lib/client/components/ApiReference/ApiReference.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import ResourceLink from '$client/components/ResourceLink/ResourceLink.svelte';
   export let href: string;
+  export let shouldRecommendTypescript = true;
 </script>
 
 <section>
@@ -8,8 +9,14 @@
 
   <p class="c-callout-info">The best documentation is no documentation!</p>
 
-  It is recommended to utilize editor intellisense & explore in-code documentation while using the
-  package (and use <ResourceLink key="typescript" /> if possible 😉).
+  <p>
+    It is recommended to utilize editor intellisense & explore in-code documentation while using the
+    package
+    {#if shouldRecommendTypescript}
+      (and use <ResourceLink key="typescript" /> if possible 😉)
+    {/if}
+    .
+  </p>
 
   <p>
     See the extracted
