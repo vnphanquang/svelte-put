f82e0f1300565708b6867cd3e31ff4ccc9f9c8aa
Quang Phan <vnphanquang@gmail.com>
2023-07-15 19:58:18 +0700

    chore(docs): move partytown & gtag script tag out of svelte file to avoid eslint & ts parsing error

---
+++ sites/docs/src/lib/shared/utils/htmlScript.ts
@@ -0,0 +1,9 @@
+import { partytownSnippet } from '@builder.io/partytown/integration';
+
+export function createPartytownSnippetScriptTag() {
+  return `<script>${partytownSnippet}</script>`;
+}
+
+export function createGtagScriptTag(id: string) {
+  return `<script type="text/partytown" async src="https://www.googletagmanager.com/gtag/js?id=${id}"></script>`;
+}
