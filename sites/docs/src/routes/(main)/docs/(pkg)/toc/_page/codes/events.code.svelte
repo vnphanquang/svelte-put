123c6c3da0ec96e06fc51e75fca6120844eed0f3
Quang Phan <vnphanquang@gmail.com>
2023-07-15 21:05:55 +0700

    chore: reflect *EventDetails naming type change

--- sites/docs/src/routes/(main)/docs/(pkg)/toc/_page/codes/events.code.svelte
+++ sites/docs/src/routes/(main)/docs/(pkg)/toc/_page/codes/events.code.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import { toc } from '@svelte-put/toc';
-  import type { TocInitEventDetails, TocChangeEventDetails } from '@svelte-put/toc';
-  function handleTocInit(event: CustomEvent<TocInitEventDetails>) {
+  import type { TocInitEventDetail, TocChangeEventDetail } from '@svelte-put/toc';
+  function handleTocInit(event: CustomEvent<TocInitEventDetail>) {
     const { items } = event.detail;
     console.log('Extracted item', items);
   }
-  function handleTocChange(event: CustomEvent<TocChangeEventDetails>) {
+  function handleTocChange(event: CustomEvent<TocChangeEventDetail>) {
     const { activeItem } = event.detail;
     console.log('Item currently on viewport', activeItem);
   }
