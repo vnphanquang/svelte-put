c019d4c631d5b3571502e9e1b7c43b3452e0696a
Loris Sigrist <sigrist@hasch.io>
2023-07-17 09:54:34 +0200

    Remember preferred package manager

--- sites/docs/src/lib/client/components/Installation/Installation.svelte
+++ sites/docs/src/lib/client/components/Installation/Installation.svelte
@@ -1,3 +1,28 @@
+<script lang="ts" context="module">
+  import { writable } from 'svelte/store';
+
+  import { browser } from '$app/environment';
+
+  const LS_KEY = 'preferredPackageManager';
+  const PACKAGE_MANAGERS = ['npm', 'pnpm', 'yarn'] as const;
+  type PackageManager = (typeof PACKAGE_MANAGERS)[number];
+
+  let preferredPackageManager = writable<PackageManager>(
+    browser ? loadPreference() : PACKAGE_MANAGERS[0],
+  );
+
+  function loadPreference(): PackageManager {
+    //`preferred` is actually string | null, but we explicitly check afterwards.
+    //TS's type-inference is not very good around `Array.prototype.includes`
+    const preferred = localStorage.getItem(LS_KEY) as PackageManager;
+
+    return PACKAGE_MANAGERS.includes(preferred) ? preferred : PACKAGE_MANAGERS[0];
+  }
+
+  if (browser)
+    preferredPackageManager.subscribe((preference) => localStorage.setItem(LS_KEY, preference));
+</script>
+
 <script lang="ts">
   import shell from 'svelte-highlight/languages/shell';
 
@@ -5,17 +30,15 @@
 
   export let pkg: string;
   export let dev = true;
+
+  $: code = {
+    npm: `npm install ${dev ? '--save-dev ' : ''}${pkg}`,
+    pnpm: `pnpm add ${dev ? '-D ' : ''}${pkg}`,
+    yarn: `yarn add ${dev ? '-D ' : ''}${pkg}`,
+  } satisfies Record<PackageManager, string>;
 </script>
 
 <section>
   <h2>Installation</h2>
-  <Code
-    code={{
-      npm: `npm install ${dev ? '--save-dev ' : ''}${pkg}`,
-      pnpm: `pnpm add ${dev ? '-D ' : ''}${pkg}`,
-      yarn: `yarn add ${dev ? '-D ' : ''}${pkg}`,
-    }}
-    lang={shell}
-    title="terminal"
-  />
+  <Code {code} lang={shell} title="terminal" bind:variant={$preferredPackageManager} />
 </section>
