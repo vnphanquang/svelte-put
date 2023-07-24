d2ab4c3cc8a94141f64d671b8931cb4be1debc57
Quang Phan <phan.quang@4digit.jp>
2023-07-24 11:14:08 +0700

    chore: ugprade prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,3 @@
 module.exports = {
   ...require('@vnphanquang/prettierrc'),
-  pluginSearchDirs: ['.'],
 };
--- sites/docs/prettier.config.cjs
+++ sites/docs/prettier.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   ...require('@vnphanquang/prettierrc'),
   plugins: [require('prettier-plugin-svelte'), require('prettier-plugin-tailwindcss')],
-  pluginSearchDirs: false,
   tailwindConfig: './tailwind.config.cjs',
 };
