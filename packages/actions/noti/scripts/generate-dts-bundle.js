33af5af1e4529d11bcdb2da271366dd13a3202c9
Quang Phan <phan.quang@4digit.jp>
2023-08-05 11:16:07 +0700

    feat(noti): Notification component for simplifying custom portal

---
+++ packages/actions/noti/scripts/generate-dts-bundle.js
@@ -0,0 +1,9 @@
+import { createBundle } from 'dts-buddy';
+
+await createBundle({
+  project: 'tsconfig.json',
+  output: 'types/index.d.ts',
+  modules: {
+    '@svelte-put/noti': 'src/index.js',
+  },
+});
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,3 +1,4 @@
 module.exports = {
   ...require('@vnphanquang/prettierrc'),
+  plugins: [require('prettier-plugin-svelte'), require('prettier-plugin-tailwindcss')],
 };
