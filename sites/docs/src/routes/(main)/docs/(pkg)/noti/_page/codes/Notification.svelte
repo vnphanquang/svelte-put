df2be3a0767bb780cb275ba542de369b4f709a34
Quang Phan <vnphanquang@gmail.com>
2023-08-06 18:11:01 +0700

    docs(noti): render example toast to bottom right corner

--- sites/docs/src/routes/(main)/docs/(pkg)/noti/_page/codes/Notification.svelte
+++ sites/docs/src/routes/(main)/docs/(pkg)/noti/_page/codes/Notification.svelte
@@ -18,15 +18,15 @@
 
 <!-- eslint-disable svelte/valid-compile -->
 <div
-  class="relative px-4 py-2 bg-blue-200 rounded-sm not-prose shadow-lg text-dark pointer-events-auto flex items-center justify-between"
+  class="relative px-4 py-2 bg-blue-200 rounded-sm not-prose shadow-lg text-dark pointer-events-auto flex items-start md:items-center justify-between"
   class:bg-pink-300={special}
   in:fly|global={{ duration: 200, y: -20 }}
   on:mouseenter={progress.pause}
   on:mouseleave={progress.resume}
 >
   <p>Notification (variant: {notification.variant}): {content} (id = {notification.id})</p>
-  <button on:click={dismiss}>
-    <svg inline-src="google/close" width="20" height="20" />
+  <button on:click={dismiss} class="md-max:mt-0.5">
+    <svg inline-src="google/close" width="24" height="24" />
   </button>
   <div
     class="progress absolute inset-x-0 bottom-0 h-0.5 bg-blue-500 origin-left"
--- sites/docs/src/routes/(main)/docs/(pkg)/noti/_page/codes/quickStart-portal.svelte
+++ sites/docs/src/routes/(main)/docs/(pkg)/noti/_page/codes/quickStart-portal.svelte
@@ -6,6 +6,6 @@
 
 <!-- notification portal, typically setup at somewhere global like root layout -->
 <aside
-  class="fixed z-notification inset-0 p-10 pointer-events-none flex flex-col-reverse gap-4 justify-end"
+  class="fixed z-notification inset-y-0 right-0 md:left-1/2 p-10 pointer-events-none flex flex-col-reverse gap-4 justify-end md:justify-start"
   use:portal={notiStore}
 />
