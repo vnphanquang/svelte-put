4bd44ac35e66afac2773a291f327c00424e5c42b
Nicholas Costa <nicholascostadev@gmail.com>
2024-01-09 19:07:46 -0300

    docs(toc): update toc docs with new way of looping through headings

--- sites/docs/src/routes/(main)/docs/(pkg)/toc/_page/codes/quick-start-input.svelte
+++ sites/docs/src/routes/(main)/docs/(pkg)/toc/_page/codes/quick-start-input.svelte
@@ -8,9 +8,9 @@
 
   <section>
     <h2>Table of Contents</h2>
-    {#if Object.values($tocStore.items).length}
+    {#if $tocStore.items.size}
       <ul>
-        {#each Object.values($tocStore.items) as tocItem}
+        {#each $tocStore.items.values() as tocItem}
           <li>
             <!-- svelte-ignore a11y-missing-attribute -->
             <!-- eslint-disable-next-line svelte/valid-compile -->
