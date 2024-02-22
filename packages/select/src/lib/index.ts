f73ec429499e017c608aa977cea2eecff67c604f
Quang Phan <vnphanquang@gmail.com>
2024-02-22 16:47:02 +0700

    chore: put back select

---
+++ packages/select/src/lib/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
+
+/**
+ * Svelte feature-rich, extensible, accessible select component
+ *
+ * @packageDocumentation
+ */
+
+export * from './Select.types';
+export { default as Select } from './Select.svelte';
