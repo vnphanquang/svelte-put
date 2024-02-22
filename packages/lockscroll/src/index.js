6bdbd81232edbebbc797c7cd6736d40d5a41e92a
Quang Phan <vnphanquang@gmail.com>
2024-02-22 10:46:14 +0700

    chore: put back lockscroll

---
+++ packages/lockscroll/src/index.js
@@ -0,0 +1,9 @@
+// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
+
+/**
+ * `use:lockscroll` - svelte action that locks scroll within an HTML element
+ *
+ * @packageDocumentation
+ */
+
+export * from './lockscroll.js';
