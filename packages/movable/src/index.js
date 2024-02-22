da35858ec2095504a374c53e000ccd78cfdf6741
Quang Phan <vnphanquang@gmail.com>
2024-02-22 10:48:45 +0700

    chore: put back movable

---
+++ packages/movable/src/index.js
@@ -0,0 +1,9 @@
+// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
+
+/**
+ * svelte action - `use:movable` move node on mousedown
+ *
+ * @packageDocumentation
+ */
+
+export * from './movable.js';
