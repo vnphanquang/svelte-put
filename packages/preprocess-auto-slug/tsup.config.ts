00e233855167a3791ef67b9458e0b8f57054cd28
Quang Phan <vnphanquang@gmail.com>
2024-02-22 17:05:32 +0700

    chore: put back preprocess-auto-slug

---
+++ packages/preprocess-auto-slug/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	entry: ['src/index.ts'],
+	outDir: 'dist',
+	format: ['cjs', 'esm'],
+	dts: true,
+	sourcemap: true,
+	splitting: false,
+	clean: true,
+	external: [],
+});
