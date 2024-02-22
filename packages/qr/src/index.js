84eac2cb38a53d670391397ca5b8d3e79bab9fae
Quang Phan <vnphanquang@gmail.com>
2024-02-22 11:46:44 +0700

    chore: put back qr

---
+++ packages/qr/src/index.js
@@ -0,0 +1,9 @@
+// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
+
+/**
+ * `use:tooltip` - svelte action to display a tooltip over an HTMLElement
+ *
+ * @packageDocumentation
+ */
+
+export { createSVG, createSVGParts, createBase64Image } from './qr/index.js';
