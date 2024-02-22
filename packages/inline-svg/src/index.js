d8553a9a39f88221367197977ba602e00701ddc3
Quang Phan <vnphanquang@gmail.com>
2024-02-22 10:08:51 +0700

    chore: put back inline-svg

---
+++ packages/inline-svg/src/index.js
@@ -0,0 +1,9 @@
+// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
+
+/**
+ * solution (action & preprocessor) for inlining SVGs in Svelte land
+ *
+ * @packageDocumentation
+ */
+
+export * from './inline-svg.action.js';
