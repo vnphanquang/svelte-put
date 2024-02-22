08c868417e68b963e2b5a51f4664e0a3b0b6242e
Quang Phan <vnphanquang@gmail.com>
2024-02-22 09:45:54 +0700

    chore: put back copy

---
+++ packages/copy/src/index.js
@@ -0,0 +1,10 @@
+// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
+
+/**
+ * svelte action and utilities for copying text to clipboard
+ *
+ * @packageDocumentation
+ */
+
+export * from './copy.action.js';
+export * from './copy.helpers.js';
