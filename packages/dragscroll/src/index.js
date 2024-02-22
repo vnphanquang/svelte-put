1fd5e1f364b70d2336a4be30bed321de589eb876
Quang Phan <vnphanquang@gmail.com>
2024-02-22 09:57:18 +0700

    chore: put back dragscroll

---
+++ packages/dragscroll/src/index.js
@@ -0,0 +1,9 @@
+// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
+
+/**
+ * svelte action `use:dragscroll` for drag-to-scroll behavior
+ *
+ * @packageDocumentation
+ */
+
+export * from './dragscroll.action.js';
