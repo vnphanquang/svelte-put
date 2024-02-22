47d9ab8a41921f29040bdee482e0a8f60a4d863e
Quang Phan <vnphanquang@gmail.com>
2024-02-22 11:22:04 +0700

    chore: put back avatar

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 export default {
 	'*.{ts,tsx,js,jsx,cjs}': ['eslint', 'prettier --write'],
-	'*.svelte': ['eslint', 'stylelint', 'prettier --write'],
+	'*.svelte': ['svelte-check', 'eslint', 'stylelint', 'prettier --write'],
 	'*.css': ['stylelint', 'prettier --write'],
 	'*.{md,json,html}': 'prettier --write',
 };
