fcc2b4edfbb345a6b519751a11d97a0b18ab7fd3
Quang Phan <vnphanquang@gmail.com>
2024-02-24 18:15:31 +0700

    refactor(docs): simplified home page

--- sites/docs/src/routes/+page.svelte
+++ sites/docs/src/routes/+page.svelte
@@ -1,7 +1,21 @@
-<script>
-
+<script lang="ts">
 </script>
 
-<main>
-	<h1 class="c-text-h1">svelte-put</h1>
+<main class="h-dvh flex items-center flex-col justify-center gap-10">
+	<div class="flex items-end gap-6">
+		<svg inline-src="svelte-put" height="100" width="100" />
+		<h1 class=" text-4xl c-text-h1 font-fingerpaint text-gradient-brand">svelte-put</h1>
+	</div>
+	<p>
+		Useful
+		<a class="c-link" href="https://svelte.dev">svelte</a> to put in your projects
+	</p>
+	<div class="flex items-center justify-between gap-10 uppercase text-sm">
+		<a class="c-link c-link--preserved text-link" href="/docs">
+			Read Docs
+		</a>
+		<a class="c-link c-link--preserved text-link" href="https://github.com/vnphanquang/svelte-put">
+			See Github
+		</a>
+	</div>
 </main>
--- sites/docs/tailwind.config.js
+++ sites/docs/tailwind.config.js
@@ -1,7 +1,28 @@
 import sveltevietnam from '@sveltevietnam/ui/css/tailwind';
+import definePlugin from 'tailwindcss/plugin.js';
 
 /** @type {import("tailwindcss").Config } */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts,md}', 'svelte.config.js'],
 	presets: [...sveltevietnam],
+	plugins: [
+		definePlugin(({ addUtilities }) => {
+			addUtilities({
+				'.bg-gradient-brand': {
+					'@apply bg-gradient-to-r from-[#ef4623] to-[#42b883]': {},
+				},
+				'.text-gradient-brand': {
+					'@apply w-fit bg-gradient-brand bg-clip-text text-transparent': {},
+				},
+			})
+		}, {
+			theme: {
+				extend: {
+					fontFamily: {
+						fingerpaint: ['Finger Paint', 'sans-serif'],
+					},
+				},
+			},
+		}),
+	]
 };
