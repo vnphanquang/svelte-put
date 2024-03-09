617749fb8f9a91a1c3f0b7d86b3722336c20dc36
Quang Phan <vnphanquang@gmail.com>
2024-03-09 19:07:58 +0700

    refactor(docs): revamped @svelte-put/lockscroll docs page

---
+++ sites/docs/src/routes/docs/(package)/lockscroll/_page/examples/quick-start.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { lockscroll } from '@svelte-put/lockscroll';
+
+  let locked = false;
+  function toggleLockScroll() {
+    locked = !locked;
+  }
+</script>
+
+<svelte:body use:lockscroll={locked} />
+<button class="c-btn mx-auto" on:click={toggleLockScroll}>Toggle lock scroll on body</button>
