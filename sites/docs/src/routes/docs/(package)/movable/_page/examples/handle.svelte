059ae8a5e09c4ea17a8d37b3b94b3c1e07bb2f19
Quang Phan <vnphanquang@gmail.com>
2024-03-12 11:05:42 +0700

    refactor(docs): revamped @svelte-put/movable docs page

---
+++ sites/docs/src/routes/docs/(package)/movable/_page/examples/handle.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { movable } from '@svelte-put/movable';
+
+  let handle: HTMLElement;
+</script>
+
+<div class="flex h-40 w-40 flex-col bg-blue-200 p-2 text-black" use:movable={{ handle }}>
+  <div class="grid h-8 w-8 place-items-center self-end bg-green-200" bind:this={handle}>.</div>
+  <div class="grid flex-1 place-items-center self-stretch">...</div>
+</div>
---
+++ sites/docs/src/routes/docs/(package)/movable/_page/examples/limit-screen.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { movable } from '@svelte-put/movable';
+</script>
+
+<div
+  class="grid h-20 w-20 place-items-center bg-blue-200 text-black"
+  use:movable={{ limit: { parent: 'screen' } }}
+>
+  ...
+</div>
