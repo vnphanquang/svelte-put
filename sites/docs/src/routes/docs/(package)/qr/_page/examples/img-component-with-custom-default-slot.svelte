e82564765a2bdce3cd15362b48559a1e366b51ea
Quang Phan <vnphanquang@gmail.com>
2024-03-12 11:42:15 +0700

    refactor(docs): revamped @svelte-put/qr docs page

---
+++ sites/docs/src/routes/docs/(package)/qr/_page/examples/img-component-with-custom-default-slot.svelte
@@ -0,0 +1,16 @@
+<script>
+  import QR from '@svelte-put/qr/img/QR.svelte';
+</script>
+
+<QR
+  data="https://svelte.dev"
+  logo="https://raw.githubusercontent.com/sveltejs/branding/master/svelte-logo.svg"
+  logoRatio={107 / 128}
+  shape="square"
+	anchorInnerFill="tomato"
+	anchorOuterFill="tomato"
+	moduleFill="tomato"
+  let:src
+>
+  <img {src} alt="qr" />
+</QR>
