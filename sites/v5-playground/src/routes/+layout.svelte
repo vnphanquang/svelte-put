7e8711f5e168ed7bf46981d5638438cd79053386
Quang Phan <vnphanquang@gmail.com>
2024-05-17 16:16:40 +0700

    chore: bootstrap a svelte v5 playground

---
+++ sites/v5-playground/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '$lib/app.css';
+</script>
+
+<slot />
---
+++ sites/v5-playground/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts,md}', 'svelte.config.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
