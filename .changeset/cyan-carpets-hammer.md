5a6d39686dab6b2774d31ebf8e3661ac37ea2aca
Quang Phan <vnphanquang@gmail.com>
2024-05-18 16:18:16 +0700

    chore(preprocess-inline-svg): return undefined during zimmerframe walking as no AST was transformed

---
+++ .changeset/cyan-carpets-hammer.md
@@ -0,0 +1,5 @@
+---
+"@svelte-put/preprocess-inline-svg": patch
+---
+
+not necessary to return if not transfer in zimmerframe walk methods
--- packages/preprocess-inline-svg/src/index.js
+++ packages/preprocess-inline-svg/src/index.js
@@ -1,9 +1,10 @@
 import { resolveInlineSvgConfig, resolveSources, transform } from './internals.js';
 
 /**
+ * create a preprocessor that inlines SVG from disk to source code at compile time
  * @param {import('./types').SourceConfig | import('./types').SourceConfig[]} [sources] - sources to search for svg
  * @param {import('./types').InlineSvgConfig} [config] - global options to configure preprocessor input & output
- * @returns {import('svelte/compiler').PreprocessorGroup}
+ * @returns {import('svelte/compiler').PreprocessorGroup} - Svelte preprocessor interface
  */
 export function inlineSvg(sources, config) {
 	const rSources = resolveSources(sources);
