24648fef185ec89896cfd4333c26c6327244f5ee
Quang Phan <vnphanquang@gmail.com>
2024-06-03 10:18:41 +0700

    chore: remove v5-playground site

--- sites/v5-playground/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts,md}', 'svelte.config.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
