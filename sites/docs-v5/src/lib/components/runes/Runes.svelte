aac7f96eebff527e9800135e2069b0a37b0c83fe
Quang Phan <vnphanquang@gmail.com>
2024-06-05 00:41:05 +0700

    feat(docs): add Runes component with experimental popover API

---
+++ sites/docs-v5/src/lib/components/runes/Runes.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	let { ...rest }: HTMLAttributes<HTMLDivElement> = $props();
+</script>
+
+<div {...rest}>
+	<button popovertarget="rune-popover" type="button" class="hover:text-link transition-colors">
+		<svg data-inline-src="runes" width="80" height="80"></svg>
+	</button>
+	<!-- TODO: enhance with floating-ui and/or @svelte-put/tooltip -->
+	<div id="rune-popover" popover="auto" class="rounded bg-bg-200 text-fg py-2 px-4 not-prose text-sm backdrop:bg-bg backdrop:opacity-50 relative">
+		<p>
+			Compatible with or powered directly by
+			<a href="https://svelte.dev/blog/runes" class="c-link">Svelte runes</a>.
+		</p>
+	</div>
+</div>
