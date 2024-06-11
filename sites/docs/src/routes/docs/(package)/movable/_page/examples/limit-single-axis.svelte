d9d9e74a7a2fa53124ca22b2c1d42e076009999d
Quang Phan <vnphanquang@gmail.com>
2024-06-11 18:04:54 +0700

    docs(movable): migrate docs site to Svelte 5

---
+++ sites/docs/src/routes/docs/(package)/movable/_page/examples/limit-single-axis.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { movable } from '@svelte-put/movable';
+</script>
+
+<div
+  class="grid h-20 w-20 place-items-center bg-blue-200 dark:text-black"
+  use:movable={{ limit: { delta: { x: '100%', y: 0 } } }}
+>
+  ...
+</div>
