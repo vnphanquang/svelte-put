c9161434d734c86b65525c7bb5a8cdaf95407d7c
Quang Phan <vnphanquang@gmail.com>
2024-06-13 21:01:48 +0700

    docs(qr): migrate docs page to Svelte 5

---
+++ sites/docs/src/routes/docs/(package)/qr/_page/examples/img-headless.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { createQrPngDataUrl } from '@svelte-put/qr';
+	import { onMount } from 'svelte';
+
+	const config = {
+		data: 'https://svelte.dev',
+		width: 500,
+		height: 500,
+		backgroundFill: '#fff',
+	}
+
+	let src = '';
+	onMount(async () => {
+		src = await createQrPngDataUrl(config);
+	});
+</script>
+
+<div class="flex items-center gap-2 flex-col">
+	<img {src} width="180" height="180" alt="a qr code" />
+	<a class="c-btn" href={src} download="qr.png">Download QR as PNG</a>
+</div>
---
+++ sites/docs/src/routes/docs/(package)/qr/_page/examples/svg-headless.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { createQrSvgString, createQrSvgDataUrl } from '@svelte-put/qr';
+
+	const config = { data: 'https://svelte.dev' }
+
+	const dataURL = createQrSvgDataUrl(config);
+	const svgString = createQrSvgString(config);
+</script>
+
+<div class="flex items-center gap-2 flex-col">
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html svgString}
+	<a class="c-btn" href={dataURL} download="qr.svg">Download QR as SVG</a>
+</div>
