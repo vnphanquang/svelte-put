bd938416644767594849d38f75498073e3bffe39
Quang Phan <vnphanquang@gmail.com>
2024-06-13 21:01:48 +0700

    docs(qr): migrate docs page to Svelte 5

---
+++ sites/docs/src/routes/docs/(package)/qr/_page/examples/img-background-fill.svelte
@@ -0,0 +1,13 @@
+<script>
+  import QR from '@svelte-put/qr/img/QR.svelte';
+</script>
+
+<QR
+  data="https://svelte-put.vnphanquang.com/docs/qr"
+  anchorOuterFill="blue"
+  moduleFill="green"
+  anchorInnerFill="blue"
+	backgroundFill="lightblue"
+	width="168"
+	height="168"
+/>
---
+++ sites/docs/src/routes/docs/(package)/qr/_page/examples/svg-action.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { qr } from '@svelte-put/qr/svg';
+</script>
+
+<svg
+  use:qr={{
+    data: 'https://svelte-put.vnphanquang.com/docs/qr',
+    logo: 'https://svelte-put.vnphanquang.com/images/svelte-put-logo.svg',
+    shape: 'circle',
+  }}
+/>
