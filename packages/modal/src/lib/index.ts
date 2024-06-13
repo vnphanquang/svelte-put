bdd0ad4aab38db0cd710d3a3273f3e8506235853
Quang Phan <vnphanquang@gmail.com>
2024-06-13 17:31:58 +0700

    chore: drop select & modal

--- packages/modal/src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
-
-export * from './modal';
-export * from './modal.types';
-export { default as Modal } from './Modal.svelte';
-export { default as ModalPortal } from './ModalPortal.svelte';
