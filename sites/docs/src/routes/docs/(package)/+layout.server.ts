5a8c95d77815f9de032b6cbc4f8b3b69bef378f9
Quang Phan <vnphanquang@gmail.com>
2024-06-15 15:07:52 +0700

    docs(preprocess-inline-svg): add deprecated docs page, point to inline-svg

--- sites/docs/src/routes/docs/(package)/+layout.server.ts
+++ sites/docs/src/routes/docs/(package)/+layout.server.ts
@@ -1,13 +1,19 @@
 import { error } from '@sveltejs/kit';
 
-import { packages, type PackageId, type Package } from '$lib/data/packages';
+import { packages, type Package, deprecatedPackages } from '$lib/data/packages';
 
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = ({ url }) => {
-	const pkgId = (url.pathname.split('/').at(-1) ?? '') as PackageId;
-	const pkg = packages[pkgId] as Package;
-	if (!pkg) error(404, 'No such package');
+	const pkgId = (url.pathname.split('/').at(-1) ?? '');
+	let pkg: Package;
+	if (pkgId in packages) {
+		pkg = packages[pkgId as keyof typeof packages];
+	} else if (pkgId in deprecatedPackages) {
+		pkg = deprecatedPackages[pkgId as keyof typeof deprecatedPackages];
+	} else {
+		error(404, 'No such package');
+	}
 	return {
 		package: pkg,
     meta: {
