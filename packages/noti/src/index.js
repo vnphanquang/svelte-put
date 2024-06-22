ea9ae84f223b9b7b0565d71f2a098b62b05e6f1d
Quang Phan <vnphanquang@gmail.com>
2024-06-22 18:28:23 +0700

    chore: drop noti source code

--- packages/noti/src/index.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Quang Phan. All rights reserved. Licensed under the MIT license.
-
-export { controller, NotificationControllerBuilder } from './notification-controller-builder.js'
-export { Notification } from './notification.svelte.js';
-export { NotificationController } from './notification-controller.svelte.js';
