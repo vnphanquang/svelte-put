a3766200e7a5bfe52d25b0f0e674442ec521631f
Quang Phan <vnphanquang@gmail.com>
2024-06-23 18:58:47 +0700

    fix(async-stack): export correct types for public API

---
+++ .changeset/warm-horses-swim.md
@@ -0,0 +1,5 @@
+---
+'@svelte-put/async-stack': patch
+---
+
+better public exported API
--- packages/async-stack/src/stack-builder.js
+++ packages/async-stack/src/stack-builder.js
@@ -1,19 +1,19 @@
 import { Stack } from './stack.svelte.js';
 
 /**
- * @template {Record<string, import('svelte').Component>} [VariantMap={}]
+ * @template {Record<string, import('svelte').Component<any>>} [VariantMap={}]
  */
 export class StackBuilder {
-	/** @type {Record<string, import('./types').StackItemVariantConfig<string, import('svelte').Component>>} */
+	/** @type {Record<string, import('./types.package').StackItemVariantConfig<string, import('svelte').Component<any>>>} */
 	#variantConfigMap = {};
 
 	/**
-	 * @type {import('./types').StackItemCommonConfig<string, import('svelte').Component> | undefined}
+	 * @type {import('./types.package').StackItemCommonConfig<string, import('svelte').Component<any>> | undefined}
 	 */
 	#init;
 
 	/**
-	 * @param {import('./types').StackItemCommonConfig<string, import('svelte').Component>} [init]
+	 * @param {import('./types.package').StackItemCommonConfig<string, import('svelte').Component<any>>} [init]
 	 */
 	constructor(init) {
 		this.#init = init;
@@ -22,9 +22,9 @@ export class StackBuilder {
 	/**
 	 * add config for a stack item variant
 	 * @template {string} Variant
-	 * @template {import('svelte').Component} UserComponent
+	 * @template {import('svelte').Component<any>} UserComponent
 	 * @param {Variant} variant
-	 * @param {UserComponent | Omit<import('./types').StackItemVariantConfig<Variant, UserComponent>, 'variant'>} config
+	 * @param {UserComponent | Omit<import('./types.package').StackItemVariantConfig<Variant, UserComponent>, 'variant'>} config
 	 * @returns {StackBuilder<VariantMap & Record<Variant, UserComponent>> }
 	 */
 	addVariant(variant, config) {
@@ -52,7 +52,7 @@ export class StackBuilder {
 }
 
 /**
- * @param {import('./types').StackItemCommonConfig<string, import('svelte').Component>} [init]
+ * @param {import('./types.package').StackItemCommonConfig<string, import('svelte').Component>} [init]
  * @returns {StackBuilder}
  */
 export function stack(init) {
