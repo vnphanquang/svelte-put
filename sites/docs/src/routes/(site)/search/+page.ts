ba9f9413db2d7f0d621b488b3c91fbc9dc442f7e
Quang Phan <vnphanquang@gmail.com>
2024-11-19 12:17:30 +0700

    chore: add back search page

---
+++ sites/docs/src/routes/(site)/search/+page.ts
@@ -0,0 +1,12 @@
+import type { PageLoad } from './$types';
+
+export const prerender = false;
+
+export const load: PageLoad = ({ url }) => {
+	return {
+		query: url.searchParams.get('q') || '',
+		meta: {
+			title: 'Search | @svelte-put',
+		},
+	};
+};
