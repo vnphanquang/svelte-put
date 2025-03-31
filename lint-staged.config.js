7648a9cd71f0070b0134d85c89515c89ac0464f1
Quang Phan <vnphanquang@gmail.com>
2025-03-31 16:13:13 +0700

    chore: remove obsolete lint-staged config (now using lefthook)

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-	'*.{ts,tsx,js,jsx,cjs}': ['eslint', 'prettier --write'],
-	'*.svelte': ['svelte-check --ignore **/*.md.svelte', 'eslint', 'stylelint', 'prettier --write'],
-	'*.css': ['stylelint', 'prettier --write'],
-	'*.{md,json,html}': 'prettier --write',
-};
